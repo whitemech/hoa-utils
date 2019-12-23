3528f37d6c313f2d075933a963374fd73ad71698
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2019-12-23 15:19:13 +0100

    update users, owners and config files to logics4ai

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
